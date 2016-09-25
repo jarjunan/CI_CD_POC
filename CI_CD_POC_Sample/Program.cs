c5323fe8410421854d86aa5b0edd8fa796fd9380
jarjunan <jayashankar.arjunan@hpe.com>
2016-09-25 10:53:09 +1000

    new c# .Net sample code
    demo .Net sample project

---
+++ CI_CD_POC_Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI_CD_POC_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to CICD POC Sample application");
+        }
+    }
+}
